2c3626211de5ac2651c2be32c4f073847c7a0c72
daniel <danttaz11@gmail.com>
2025-12-07 17:16:26 -0300

    first commit

---
+++ src/test/java/com/Projeto/ecommerce_cart/EcommerceCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Projeto.ecommerce_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
