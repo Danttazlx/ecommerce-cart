054041895ee5e4bb6ec5ff7798b7c3124abebccf
daniel <danttaz11@gmail.com>
2025-12-18 17:08:01 -0300

    exception personalizada criada

---
+++ src/main/java/com/Projeto/ecommerce_cart/exception/DeleteNotFoundException.java
@@ -0,0 +1,7 @@
+package com.Projeto.ecommerce_cart.exception;
+
+public class DeleteNotFoundException extends RuntimeException {
+    public DeleteNotFoundException(String message) {
+        super(message);
+    }
+}
